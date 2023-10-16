1250e753e9ec65d6af959d2b429958d9e3ddd5f2
Ignacio Kingston <kingston.ignacio2@gmail.com>
2023-10-16 14:06:45 +0200

    Portfolio push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import FicheProjet from './pages/FicheProjet';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const root = createRoot(document.getElementById('root'));
+
+root.render (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element= {<Home/>}/>
+        <Route path="/ficheprojets/:id" element= {<FicheProjet/>}/>
+        <Route path="/about" element= {<About/>}/>
+        <Route path="/contact" element= {<Contact/>}/>
+        <Route path="/*" element= {<NotFound/>}/>
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>,
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
